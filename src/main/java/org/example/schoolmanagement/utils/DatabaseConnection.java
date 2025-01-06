3e475275638a69f6b027ea310d7562010342df06
HungDao03 <daohung151003@gmail.com>
2025-01-06 11:34:35 +0700

    Cập nhật DatabaseConnection.java và dataSources.xml

--- src/main/java/org/example/schoolmanagement/utils/DatabaseConnection.java
+++ src/main/java/org/example/schoolmanagement/utils/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/student_manager";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "Nf!23456";
+    private static final String PASSWORD = "123456";
 
     // Static method to get a connection
     public static Connection getConnection() throws SQLException {
